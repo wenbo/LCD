571b942f714c717df300225aa9c74b63ad9603fc
bob <wen_bo@kembo-net.com>
2013-06-02 09:55:17 +0800

    now UL one's html ok

---
+++ filterfactor.datalist_1.js
@@ -0,0 +1,9 @@
+filterfactor.datalist = {
+  tGetArray: function()
+  {
+    var tArray = [];
+    tArray[0] = {'-category-product-folder':'products/LCD-70X55A','-category-product-abs-folder':'viera/TH-L58E60C','notclickable':'0','-notclickable-text':'了解产品','property_topid':'0000','property_topid_filetype':'png','topic':'LCD-70X55A','subtopic':'70英寸<br />','state':'100','filterfactor':'1_____VALUES_____70_____PARAMETERS_____2_____VALUES_____1,2,3,5,6,7,8_____PARAMETERS_____3_____VALUES_____1_____PARAMETERS_____4_____VALUES_____4_____PARAMETERS_____5_____VALUES_____2'};
+    tArray[1] = {'-category-product-folder':'viera/TH-L32X30C','-category-product-abs-folder':'viera/TH-L32X30C','notclickable':'0','-notclickable-text':'了解产品','property_topid':'65','property_topid_filetype':'png','topic':'X30系列','subtopic':'32英寸<br />纤薄机身，精彩分享','state':'0','filterfactor':'19_____VALUES_____32_____PARAMETERS_____20_____VALUES_____'};
+    return tArray;
+  }
+};
